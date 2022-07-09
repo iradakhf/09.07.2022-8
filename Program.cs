d9e1df0dbe7b954843753e695aedd1679f935909
iradakhf <iradakhf@code.edu.az>
2022-07-09 10:42:08 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ConsoleApp16
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your task is to create a function that does four basic mathematical operations.
+
+            //The function should take three arguments -operation(string / char), value1(number), value2(number).
+            //The function should return result of numbers after applying the chosen operation.
+            int num = performTheGivenOperation(4, 5, '+');
+            Console.WriteLine(num);
+            
+        }
+        
+        public static int performTheGivenOperation(int number1, int number2, char i)
+        {
+            int operation=0;
+
+            switch (i)
+            {
+                case '+':
+                    operation =number1 + number2;
+                    break;
+                case '-':
+                    operation = number1 - number2;
+                    break;
+                case '*':
+                    operation = number1 * number2;
+                    break;
+                case '/':
+                    operation = number1 / number2;
+                    break;
+            }
+            return operation;
+        }
+    }
+}
+
